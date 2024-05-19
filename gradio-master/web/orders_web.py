851bd1348252efd70cc91668f65f0d0de92f414f
yanqing-zhang <zhangyanqing2023@126.com>
2024-05-19 16:08:38 +0800

    修改测试数据

--- gradio-master/web/orders_web.py
+++ gradio-master/web/orders_web.py
@@ -19,7 +19,7 @@ def get_orders():
     return data
 def launch_gradio():
     df = pd.DataFrame({
-        "A": [14, 4, 5, 4, 1],
+        'Fruit': ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'],
         "B": [5, 2, 54, 3, 2],
         "C": [20, 20, 7, 3, 8],
         "D": [14, 3, 6, 2, 6],
